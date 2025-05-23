eacfd2c4143f28e5699f531f08132f354e992cde
ag-covamat <a.grossmann@toutfaire.pro>
2025-05-23 15:39:15 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function (e) {
-  e.preventDefault();
-  document.getElementById("formMessage").textContent = "Thanks for your message! (This is a demo form)";
-});
